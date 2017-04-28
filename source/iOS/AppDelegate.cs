dc1df3bbd106b700f136f8d986bff376d5dae5d0
道化師 <wraith@trickpalace.net>
2017-04-28 13:54:39 +0900

    🛠 iOS版の OmegaFactory ( WIP )

--- source/iOS/AppDelegate.cs
+++ source/iOS/AppDelegate.cs
@@ -10,13 +10,24 @@ namespace seven.trumpets.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        AlphaApp App;
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
-
-            LoadApplication(new App());
+            LoadApplication(MakeSureApp());
 
             return base.FinishedLaunching(app, options);
+        }
+        public AlphaApp MakeSureApp()
+        {
+            if (null == App)
+            {
+                global::Xamarin.Forms.Forms.Init();
+                //ImageCircleRenderer.Init();
+                OmegaFactory.MakeSureInit();
+                App = AlphaFactory.MakeSureApp();
+            }
+        return App;
         }
     }
 }
